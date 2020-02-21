64f6b25a8d97894c423738ce7efd00556a8c3dca
KauanPeres <50154037+KauanPeres@users.noreply.github.com>
2020-02-21 20:16:44 -0300

    Implantado a busca por nome e número

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,69 +9,89 @@ export class HomePage {
 
   public listaPokemons = [
     {
-    numero: '001', 
-    nome: 'Bulbassaur', 
-    tipos: ['Grass', 'Poison'], 
-    img: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png'
+      numero: '001',
+      nome: 'Bulbassaur',
+      tipos: ['Grass', 'Poison'],
+      img: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png'
     },
     {
-    numero: '004', 
-    nome: 'Charmander', 
-    tipos: ['Fire'], 
-    img: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png'
+      numero: '004',
+      nome: 'Charmander',
+      tipos: ['Fire'],
+      img: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png'
     },
     {
-    numero: '007', 
-    nome: 'Squirtle', 
-    tipos: ['Water'], 
-    img: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png'
+      numero: '007',
+      nome: 'Squirtle',
+      tipos: ['Water'],
+      img: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png'
     },
     {
-      numero: '010', 
-      nome: 'Caterpie', 
-      tipos: ['Bug'], 
+      numero: '010',
+      nome: 'Caterpie',
+      tipos: ['Bug'],
       img: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/010.png'
     },
     {
-      numero: '013', 
-      nome: 'Weedle', 
-      tipos: ['Bug', 'Poison'], 
+      numero: '013',
+      nome: 'Weedle',
+      tipos: ['Bug', 'Poison'],
       img: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/013.png'
     },
     {
-      numero: '016', 
-      nome: 'Pidgey', 
-      tipos: ['Normal', 'Flying'], 
+      numero: '016',
+      nome: 'Pidgey',
+      tipos: ['Normal', 'Flying'],
       img: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png'
     },
     {
-      numero: '019', 
-      nome: 'Rattata', 
-      tipos: ['Normal'], 
+      numero: '019',
+      nome: 'Rattata',
+      tipos: ['Normal'],
       img: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/019.png'
     },
     {
-      numero: '021', 
-      nome: 'Spearow', 
-      tipos: ['Normal', 'Flying'], 
+      numero: '021',
+      nome: 'Spearow',
+      tipos: ['Normal', 'Flying'],
       img: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/021.png'
     },
     {
-      numero: '023', 
-      nome: 'Ekans', 
-      tipos: ['Poison'], 
+      numero: '023',
+      nome: 'Ekans',
+      tipos: ['Poison'],
       img: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/023.png'
     },
     {
-      numero: '025', 
-      nome: 'Pikachu', 
-      tipos: ['Electric'], 
+      numero: '025',
+      nome: 'Pikachu',
+      tipos: ['Electric'],
       img: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png'
     }
   ];
 
   public listaFiltrada = [];
 
-  constructor() {}
+  constructor() {
+    this.resetarLista();
+  }
 
+  private resetarLista() {
+    this.listaFiltrada = this.listaPokemons;
+  }
+
+  public buscarPokemon(evento: any) {
+    let busca = evento.target.value;
+
+    this.resetarLista();
+
+    if (busca && busca.trim() !== '') {
+      this.listaFiltrada = this.listaFiltrada.filter(dados => {
+        if ((dados.nome.toLowerCase().indexOf(busca.toLowerCase()) > -1) || (dados.numero.toLowerCase().indexOf(busca.toLowerCase()) > -1)) {
+          return true;
+        }
+        return false;
+      });
+    }
+  }
 }
